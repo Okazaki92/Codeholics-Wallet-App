92829baff3ee73f959df70a444f0a6474726b4d6
Okazaki92 <vincentslominski@gmail.com>
2023-09-25 17:28:31 +0200

    fix/ usuenięcie base name do terstów

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
