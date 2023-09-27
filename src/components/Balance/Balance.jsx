76c16903fceb5991f25c39c451c1b9401afb779e
Dorota Dudzik <113298700+doradudzik@users.noreply.github.com>
2023-09-27 16:08:17 +0200

    Balance
    -Balance component, style do niego.
    -Funkcja do formatowania liczby w totalBalance
    -usunęłam zakomentowany kod w RegistrationPage

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,9 +1,26 @@
-export const Balance = () => {
+import { useEffect } from "react";
+import css from "./Balance.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setBalance } from "../../redux/transactions/transactionSlice";
+import formatNumberWithSpaces from "../../utils/formatBalanceNumber";
+
+const Balance = () => {
+  const dispatch = useDispatch();
+  const totalBalance = useSelector((state) => state.transactions.totalBalance);
+
+  useEffect(() => {
+    dispatch(setBalance(totalBalance));
+  }, [dispatch, totalBalance]);
+
   return (
-    <div>
-      <div></div>
+    <div className={css.balanceBox}>
+      <p className={css.headerVisibility}>YOUR BALANCE</p>
+      <div className={css.balanceBox__content}>
+        <p>$</p>
+        <p>{formatNumberWithSpaces(totalBalance)}</p>
+      </div>
     </div>
   );
 };
 
-
+export default Balance;
