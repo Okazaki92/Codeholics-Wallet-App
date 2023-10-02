a9a077c8d3e9eeed69ddf0b1bc69f83b094eb468
Dorota Dudzik <113298700+doradudzik@users.noreply.github.com>
2023-10-02 18:26:03 +0200

    TeamModal-css
    CSS i zmieniłam żeby już wszystko było po angielsku(przyciski, nazwa zespołu itd.)

--- src/components/TeamMember/TeamMember.jsx
+++ src/components/TeamMember/TeamMember.jsx
@@ -1,3 +1,4 @@
+import styles from "../Footer/Footer.module.css";
 const TeamMember = ({
   name,
   avatar,
@@ -9,36 +10,52 @@ const TeamMember = ({
   github,
 }) => {
   return (
-    <div>
-      <h2>
+    <div className={styles.teamInfo}>
+      <h2 className={styles.headerName}>
         {name} - {role}
       </h2>
       <img src={avatar} alt="avatar" className={name} />
-      <h3>Project Goals:</h3>
+      <h3 className={styles.header}>Project Goals:</h3>
       <ul>
         {goals.map((goal, index) => (
           <li key={index}>{goal}</li>
         ))}
       </ul>
-      <h3>Project Difficulties:</h3>
+      <h3 className={styles.header}>Project Difficulties:</h3>
       <ul>
         {difficulties.map((difficulty, index) => (
           <li key={index}>{difficulty}</li>
         ))}
       </ul>
-      <h3>Hobbies:</h3>
+      <h3 className={styles.header}>Hobbies:</h3>
       <ul>
         {hobbies.map((hobby, index) => (
           <li key={index}>{hobby}</li>
         ))}
       </ul>
-      <h3>My Socials</h3>
-      <a href={linkedin} target="_blank" rel="noopener noreferrer">
-        LinkedIn Profile
-      </a>
-      <a href={github} target="_blank" rel="noopener noreferrer">
-        GitHub Profile
-      </a>
+      <h3 className={styles.header}>My Socials</h3>
+      <ul>
+        <li>
+          <a
+            className={styles.link}
+            href={linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Profile
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles.link}
+            href={github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
